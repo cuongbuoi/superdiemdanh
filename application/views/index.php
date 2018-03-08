46fb1e0552c058acc22d588958c7998fb07ef447
Thuanmap <hjtshort@gmail.com>
2018-03-08 21:46:25 +0700

    buoi loz

--- application/views/index.php
+++ application/views/index.php
@@ -6,8 +6,8 @@
     <title>Chào mừng đến với Quản lí điểm danh</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-   <link href="<?php echo base_url('../application/assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
-   <link href="<?php echo base_url('../application/assets/css/style.css'); ?>" rel="stylesheet" type="text/css" />
+   <link href="application/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+   <link href="application/assets/css/style.css" rel="stylesheet" type="text/css" />
     <script src="main.js"></script>
 </head>
 <body>
