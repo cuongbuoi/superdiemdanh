4c89cdd7cf18b14f3a3d48117e293cd7deeff798
cuongbuoi <cuongyo19997@gmail.com>
2018-03-08 21:12:11 +0700

    map loz

--- application/views/index.php
+++ application/views/index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Chào mừng đến với Quản lí điểm danh</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>application/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>/application/assets/css/style.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    Tao là thựn
+    <p class="chomap">Chó</p>
 </body>
 </html>
