e0582b789b8b2e503780f850d27382232a2199d9
Phuoc <kaisai@ceh.vn>
2018-03-08 21:31:48 +0700

    fix bug ngu cua thuan va cuong

--- application/views/index.php
+++ application/views/index.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Chào mừng đến với Quản lí điểm danh</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>application/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>/application/assets/css/style.css" />
+    
+   <link href="<?php echo base_url('../application/assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
+   <link href="<?php echo base_url('../application/assets/css/style.css'); ?>" rel="stylesheet" type="text/css" />
     <script src="main.js"></script>
 </head>
 <body>
