591a82b8e9528aa1dc4e1517bf262b5b1a1444fd
cuongbuoi <cuongyo19997@gmail.com>
2018-03-09 08:57:16 +0700

    xong login

--- application/views/index.php
+++ application/views/index.php
@@ -8,9 +8,32 @@
     
    <link href="application/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
    <link href="application/assets/css/style.css" rel="stylesheet" type="text/css" />
-    <script src="main.js"></script>
+    <script src="application/assets/jquery.js"></script>
 </head>
-<body>
-    <p class="chomap">Chó</p>
+<body class="login-body">
+    <div class="d-flex  justify-content-center">
+        <div class="col-md-5 mt-5">
+            <div class="card card-login">
+                <div class="card-body">
+                    <h1 class="text-center">Đăng nhập</h1>
+                    <form action="#">
+                        <div class="form-group">
+                            <label for="username">Tài khoản</label>
+                            <input type="text" class="form-control" placeholder="Nhập vào tài khoản của bạn">
+                        </div>
+                        <div class="form-group">
+                            <label for="pass">Mật khẩu</label>
+                            <input type="password" class="form-control" placeholder="Nhập vào mật khẩu của bạn">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input">
+                            <label class="form-check-label">Nhớ mật khẩu</label>
+                        </div>
+                        <button type="button" class="btn btn-primary btn-login float-right">Đăng nhập</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
