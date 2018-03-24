95119f35f02a86f119cb5f24d2bc8a93ae9f9170
cuongbuoi <cuongyo19997@gmail.com>
2018-03-24 16:57:59 +0700

    Xong thong ke

--- application/views/module/thongke-content.php
+++ application/views/module/thongke-content.php
@@ -1,1 +1,39 @@
-cc
+<!-- Cách đổi màu Widget: gắn class widget-màu     (màu: xanh, do, xanhdam, cam, vang, hong, tim) -->
+<div class="widget-thongke row">
+	<div class="col-md-3">
+		<div class="card text-center widget-do">
+			<div class="card-body">
+				<h3 class="card-title">Thống kê 1</h3>
+				<p class="card-title so">69</p>
+				<p class="noidung">Nhập nội dung vào đây</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="card text-center widget-xanhdam">
+			<div class="card-body">
+				<h3 class="card-title">Thống kê 2</h3>
+				<p class="card-title so">69</p>
+				<p class="noidung">Nhập nội dung vào đây</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="card text-center widget-cam">
+			<div class="card-body">
+				<h3 class="card-title">Thống kê 3</h3>
+				<p class="card-title so">69</p>
+				<p class="noidung">Nhập nội dung vào đây</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="card text-center widget-hong">
+			<div class="card-body">
+				<h3 class="card-title">Thống kê 4</h3>
+				<p class="card-title so">69</p>
+				<p class="noidung">Nhập nội dung vào đây</p>
+			</div>
+		</div>
+	</div>
+</div>
