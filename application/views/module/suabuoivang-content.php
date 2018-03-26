4a4f1f61a0ea58c434148f649cbcce20f344854e
cuongbuoi <cuongyo19997@gmail.com>
2018-03-26 14:56:53 +0700

    nho anh map gan button

--- application/views/module/suabuoivang-content.php
+++ application/views/module/suabuoivang-content.php
@@ -1,4 +1,116 @@
+<div class="modal fade" id="suabuoivang" tabindex="-1" role="dialog" aria-labelledby="suabuoivang" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Buổi vắng</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+              <!-- table -->
+                    <div class="table-responsive">
+                          <table class="table" id="edittable">
+                              <thead class="thead-light">
+                                  <tr>
+                                      <th>Họ và Tên</th>
+                                      <th>MSSV</th>
+                                      <th>Lớp</th>
+                                      <th>Môn</th>
+                                      <th>Điểm 1</th>
+                                      <th>Điểm 2</th>
+                                      <th>Điểm 3</th>
+                                      <th>Điểm TB</th>
+                                 
+                                  </tr>
+                              </thead>
+                              <tbody id="print">
+                                
+                              </tbody>
+                          </table>
+                      </div>
+                      <script src="../application/assets/js/jquery.tabledit.js"></script>
+                      <script>
+                        jQuery(document).ready(function($) {
+                          $('#sidebar').toggleClass('active');
+                        });
+                      function sub(){
+                          var idclass= $('#lop').val()
+                         $.ajax({
+                             type: "post",
+                             url: "get_sub",
+                             data: {"id":idclass},
+                             success: function (response) {
+                                 $('.tt').html(response)
+                             }
+                         });
+                      }
+                      $( document ).ready(function() {
+                        sub()
+                      });
+                      $('#lop').on('change', function() {
+                           sub()
+                      })
+                     function get(){
+                         var idclass=$('#lop').val()
+                         var idmon=$('#mon').val()
+                         $.ajax({
+                             type: "post",
+                             url: "get_table",
+                             data: {
+                                 "class":idclass,
+                                 "mon":idmon
+                             },
+                             success: function (response) {
+                                 $('#print').html(response)
+                                 edit()
+                                 $(".btn-toolbar").css("display","block");
+                             }
+                         });
+                     }
 
+                      function edit(){
+                         
+                              $('#edittable').Tabledit({
+                              url: 'edit_mark',
+                              deleteButton: false,
+                              saveButton: true,
+                              autoFocus: false,
+                              buttons: {
+                                  edit: {
+                                      class: 'btn btn-primary',
+                                      html: '<span class="glyphicon glyphicon-pencil"></span> &nbsp EDIT',
+                                      action:'edit'
+                                  },
+                                  save:{
+                                      class: 'btn btn-success',
+                                      html: '<span class="glyphicon glyphicon-pencil"></span> &nbsp SAVE',
+                                      action:'save'
+                                  }
+                              },
+                              columns: {
+                                  identifier: [1, 'mssv'],
+                                  editable: [[5, 'diem2'], [6, 'diem3']]
+                              },
+                              onSuccess: function(data, textStatus, jqXHR) {
+                                      get()
+                                 
+                                  },
+                              onFail: function(jqXHR, textStatus, errorThrown) {
+                                  console.log('onFail(jqXHR, textStatus, errorThrown)');
+                                  console.log(jqXHR);
+                                  console.log(textStatus);
+                                  console.log(errorThrown);
+                                      }
+                               });
+                      }
+                      
+                      </script>
+               <!-- table -->
+      </div>
+    </div>
+  </div>
+</div>
 <div class="row control">
     <div class="chonmon col-md-6">
         <form action="http://localhost/superdiemdanh/" method="post">
